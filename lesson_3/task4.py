74499ed2f39b5fef44c3f864608ff552823ad311
kesch9 <kashenkovsergej@yandex.ru>
2019-03-29 17:49:58 +0300

    Lesson 3 task 4 complete

--- lesson_3/task4.py
+++ lesson_3/task4.py
@@ -1,1 +1,16 @@
 # Определить, какое число в массиве встречается чаще всего.
+def maxIn(x):
+    y = dict()
+    if (type(x) == list):
+        a = set(x)
+        for i in a:
+            z = 1
+            for j in x:
+                if i == j:
+                    y[i] = z
+                    z += 1
+        return y
+    return {}
+
+x = [1,3,5646,34,233,1,3]
+print(maxIn(x))
